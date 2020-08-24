387af564799a309756c44495695db65aefb25e6d
bnslarry <604814861@qq.com>
2020-08-24 23:51:43 +0800

    test commit

--- index.js
+++ index.js
@@ -42,6 +42,29 @@ http.createServer(async (req, res) => {
       stdio:'inherit',
   })
 }
+    // 复制 Dockerfile 到项目目录
+    fs.copyFileSync(path.resolve(`./Dockerfile`), path.resolve(projectDir,'./Dockerfile'))
+
+    // 复制 .dockerignore 到项目目录
+    fs.copyFileSync(path.resolve(__dirname,`./.dockerignore`), path.resolve(projectDir, './.dockerignore'))
+
+     // 创建 docker 镜像
+    execSync(`docker build . -t ${data.repository.name}-image:latest `,{
+      stdio:'inherit',
+      cwd: projectDir
+  })
+
+     // 销毁 docker 容器
+     execSync(`docker ps -a -f "name=^${data.repository.name}-container" --format="{{.Names}}" | xargs -r docker stop | xargs -r docker rm`, {
+      stdio: 'inherit',
+  })
+
+     // 创建 docker 容器
+     execSync(`docker run -d -p 8888:80 --name ${data.repository.name}-container  ${data.repository.name}-image:latest`, {
+      stdio:'inherit',
+  })
+
+  console.log('deploy success')
     res.end('ok')
 }).listen(3000, () => {
     console.log('server is ready')
