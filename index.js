9f19eaa7bca78c313910990cdd56a70d5300d5f1
bnslarry <604814861@qq.com>
2020-08-25 00:38:29 +0800

    update

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ http.createServer(async (req, res) => {
         })
 
         // 复制 Dockerfile 到项目目录
-        fs.copyFileSync(path.resolve(__dirname, `./Dockerfile`), path.resolve('root/docker/docker-demo', './Dockerfile'))
+        fs.copyFileSync(path.resolve(__dirname, `./Dockerfile`), path.resolve(projectDir, './Dockerfile'))
 
         // 复制 .dockerignore 到项目目录
         fs.copyFileSync(path.resolve(__dirname, `./.dockerignore`), path.resolve(projectDir, './.dockerignore'))
